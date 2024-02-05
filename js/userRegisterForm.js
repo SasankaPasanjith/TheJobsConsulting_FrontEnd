af9af071c42138c6c63c1589c05d24a76ff44723
SasankaPasanjith <sasankapasanjith@gmail.com>
2024-02-05 17:55:43 +0530

    Create user registration javascript

---
+++ js/userRegisterForm.js
@@ -0,0 +1,51 @@
+let form = document.getElementById("form");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  let url = "http://localhost:8085/api/V3/registerUser";
+
+  let name = document.getElementById("formName").value;
+
+  let mobileNumber = document.getElementById("formMobile").value;
+
+  let password = document.getElementById("InputPassword").value;
+  let confirmPassword = document.getElementById("InputConfirmPassword").value; 
+
+  let email = document.getElementById("InputEmail").value;
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match. Please confirm your password.");
+    return; // Prevent submission if passwords don't match
+  }
+
+  let customerDetails = {
+    name: name,
+    mobileNo: mobileNumber,
+    password,
+    email: email,
+  };
+
+  let data = await fetch(url, {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify(customerDetails),
+  });
+
+  let data1 = await data.json();
+
+  console.log(data1);
+
+  if (!data1.errorMsg) {
+    alert(
+      "You have succefully registered. You are redirecting towards login page."
+    );
+    window.location.href = "../userLoginForm.html";
+  } else {
+    alert(data1.errorMsg);
+  }
+});
